be7dd864db15359e7b11552b128aef04f6a1f3de
Shuflduf <shuflduf@gmail.com>
2024-11-19 15:55:36 -0700

    finished adding rotations

--- src/pieces.rs
+++ src/pieces.rs
@@ -49,45 +49,145 @@ pub const PIECES: [[[U16Vec2; 4]; 4]; 7] = [
             U16Vec2::new(2, 1),
         ],
         [
-            U16Vec2::new(0, 1),
+            U16Vec2::new(0, 0),
+            U16Vec2::new(1, 0),
             U16Vec2::new(1, 1),
-            U16Vec2::new(2, 1),
-            U16Vec2::new(2, 0),
+            U16Vec2::new(1, 2),
         ],
     ],
     // O Piece (Yellow, Index 2)
     [
-        U16Vec2::new(1, 1),
-        U16Vec2::new(1, 2),
-        U16Vec2::new(2, 1),
-        U16Vec2::new(2, 2),
+        [
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+        ],
+        [
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+        ],
+        [
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+        ],
+        [
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+        ],
     ],
     // S Piece (Green, Index 3)
     [
-        U16Vec2::new(0, 1),
-        U16Vec2::new(1, 1),
-        U16Vec2::new(1, 0),
-        U16Vec2::new(2, 0),
+        [
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(2, 0),
+        ],
+        [
+            U16Vec2::new(1, 0),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+        ],
+        [
+            U16Vec2::new(0, 2),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(2, 1),
+        ],
+        [
+            U16Vec2::new(0, 0),
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+        ],
     ],
     // I Piece (Cyan, Index 4)
     [
-        U16Vec2::new(0, 1),
-        U16Vec2::new(1, 1),
-        U16Vec2::new(2, 1),
-        U16Vec2::new(3, 1),
+        [
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(3, 1),
+        ],
+        [
+            U16Vec2::new(2, 0),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(2, 2),
+            U16Vec2::new(2, 3),
+        ],
+        [
+            U16Vec2::new(0, 2),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 2),
+            U16Vec2::new(3, 2),
+        ],
+        [
+            U16Vec2::new(1, 0),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(1, 3),
+        ],
     ],
     // J Piece (Blue, Index 5)
     [
-        U16Vec2::new(0, 0),
-        U16Vec2::new(0, 1),
-        U16Vec2::new(1, 1),
-        U16Vec2::new(2, 1),
+        [
+            U16Vec2::new(0, 0),
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(2, 1),
+        ],
+        [
+            U16Vec2::new(2, 0),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+        ],
+        [
+            U16Vec2::new(2, 2),
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(2, 1),
+        ],
+        [
+            U16Vec2::new(0, 2),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(1, 1),
+            U16Vec2::new(1, 2),
+        ],
     ],
     // T Piece (Pink, Index 6)
     [
-        U16Vec2::new(0, 1),
-        U16Vec2::new(1, 0),
-        U16Vec2::new(2, 1),
-        U16Vec2::new(1, 1),
+        [
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(1, 1),
+        ],
+        [
+            U16Vec2::new(1, 2),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(1, 1),
+        ],
+        [
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(2, 1),
+            U16Vec2::new(1, 1),
+        ],
+        [
+            U16Vec2::new(0, 1),
+            U16Vec2::new(1, 0),
+            U16Vec2::new(1, 2),
+            U16Vec2::new(1, 1),
+        ],
     ]
 ];
