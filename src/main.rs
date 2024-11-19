def9d8e602c5597bab0991401e5bda080ad20504
Shuflduf <shuflduf@gmail.com>
2024-11-19 13:13:13 -0700

    moving piece

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::reflect::List;
 use rand::Rng;
 
 #[derive(Component)]
@@ -17,30 +18,46 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
     commands.spawn(Camera2dBundle::default());
+    let mut blocks: Vec<Entity> = vec![];
+    for i in 0..4 {
+        let index = rand::thread_rng().gen_range(0..12);
+        let id = commands.spawn((
+            Block {
+                col_index: index
+            },
+            SpriteBundle {
+                texture: asset_server.load("Tetr-Skin.png"),
+                transform: Transform::from_xyz((i * 31) as f32, 0.0, 0.0),
+                ..default()
+            },
+            TextureAtlas {
+                layout: asset_server.add(TextureAtlasLayout::from_grid(
+                    UVec2::splat(31),
+                    12,
+                    1,
+                    None,
+                    None
+                )),
+                index: index as usize
+            },
+        )).id();
+        blocks.push(id);
+    }
 }
 
 fn keyboard_input(
     keys: Res<ButtonInput<KeyCode>>,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    mut query: Query<&mut Transform, With<Block>>
 ) {
-    println!("{keys:?}");
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("Tetr-Skin.png"),
-            ..default()
-        },
-        TextureAtlas {
-            layout: asset_server.add(TextureAtlasLayout::from_grid(
-                UVec2::splat(31),
-                12,
-                1,
-                None,
-                None
-            )),
-            index: rand::thread_rng().gen_range(0..12),
-        },
-    ));
+    for mut transform in &mut query {
+
+        if keys.just_pressed(KeyCode::Space) {
+            transform.translation.y += -31.0;
+        }
+    }
 }
