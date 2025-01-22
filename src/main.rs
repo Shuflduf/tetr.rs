9ecddb2e7c273914133d963325cbdf23f990d5e0
Shuflduf <shuflduf@gmail.com>
2025-01-21 19:12:52 -0700

    SRS DOT JSON !!!!!!!

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use macroquad::prelude::*;
 async fn main() {
     const GRID_SIZE: IVec2 = ivec2(10, 20);
     let texture = load_texture("assets/texture_simple.png").await.unwrap();
+    texture.set_filter(FilterMode::Nearest);
 
     let bounds: Vec<IVec2> = {
         let mut final_bounds = Vec::new();
@@ -21,6 +22,8 @@ async fn main() {
 
         // Draw board
         let block_size = screen_height() / (GRID_SIZE.y as f32 + 1.0);
+        let board_width = block_size * (GRID_SIZE.x as f32 + 2.0);
+        let offset_x = (screen_width() - board_width) / 2.0;
         for block in bounds.iter() {
             let params = DrawTextureParams {
                 dest_size: Some(vec2(block_size, block_size)),
@@ -29,22 +32,25 @@ async fn main() {
             };
             draw_texture_ex(
                 &texture,
-                block.x as f32 * block_size,
+                offset_x + block.x as f32 * block_size,
                 block.y as f32 * block_size,
                 WHITE,
                 params,
             );
         }
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_rectangle(
+            0.0,
+            0.0,
+            20.0,
+            20.0,
+            YELLOW
+        );
 
         next_frame().await
     }
 }
 
 fn get_rect_from_index(index: i32) -> Option<Rect> {
-    Some(Rect::new(index as f32 * 32.0, 0.0, 32.0, 32.0))
+    Some(Rect::new(index as f32 * 32.0 + 4.0, 4.0, 26.0, 26.0))
 }
