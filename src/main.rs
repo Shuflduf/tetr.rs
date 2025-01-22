6ec12360f4625d5679e81cccf151878cfa477e41
Shuflduf <shuflduf@gmail.com>
2025-01-22 02:26:03 -0700

    very basic collision

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,63 @@
 use macroquad::prelude::*;
+use pieces::Block;
 
 mod pieces;
 
 const GRID_SIZE: IVec2 = ivec2(10, 20);
-pub static mut BLOCK_SIZE: f32 = 0.0;
-pub static mut OFFSET_X: f32 = 0.0;
 
 #[macroquad::main("MyGame")]
 async fn main() {
     pieces::load_json();
     let texture = load_texture("assets/texture_simple.png").await.unwrap();
     texture.set_filter(FilterMode::Nearest);
+    let mut collision: Vec<Block> = reset_board();
 
-    let bounds: Vec<IVec2> = {
-        let mut final_bounds = Vec::new();
-        for y in 0..(GRID_SIZE.y + 1) {
-            final_bounds.push(ivec2(0, y));
-            final_bounds.push(ivec2(GRID_SIZE.x + 1, y));
-        }
-        for x in 0..(GRID_SIZE.x) {
-            final_bounds.push(ivec2(x + 1, GRID_SIZE.y));
-        }
-        final_bounds
-    };
     loop {
         clear_background(BLACK);
 
-        unsafe {
-            // Draw board
-            BLOCK_SIZE = screen_height() / (GRID_SIZE.y as f32 + 1.0);
-            let board_width = BLOCK_SIZE * (GRID_SIZE.x as f32 + 2.0);
-            OFFSET_X = (screen_width() - board_width) / 2.0;
-            for block in bounds.iter() {
-                let params = DrawTextureParams {
-                    dest_size: Some(vec2(BLOCK_SIZE, BLOCK_SIZE)),
-                    source: get_rect_from_index(7),
-                    ..Default::default()
-                };
-                draw_texture_ex(
-                    &texture,
-                    OFFSET_X + block.x as f32 * BLOCK_SIZE,
-                    block.y as f32 * BLOCK_SIZE,
-                    WHITE,
-                    params,
-                );
-            }
+        // Draw board
+        let block_size = screen_height() / (GRID_SIZE.y as f32 + 1.0);
+        let board_width = block_size * (GRID_SIZE.x as f32 + 2.0);
+        let offset_x = (screen_width() - board_width) / 2.0;
+        for block in collision.iter() {
+            let params = DrawTextureParams {
+                dest_size: Some(vec2(block_size, block_size)),
+                source: get_rect_from_index(block.index.into()),
+                ..Default::default()
+            };
+            draw_texture_ex(
+                &texture,
+                offset_x + block.pos.x as f32 * block_size,
+                block.pos.y as f32 * block_size,
+                WHITE,
+                params,
+            );
         }
-
-        pieces::update(&texture);
-
+        pieces::update(&texture, block_size, offset_x, &mut collision);
         next_frame().await
     }
+}
+
+fn reset_board() -> Vec<Block> {
+    let mut board: Vec<Block> = Vec::new();
+    board.clear();
+    for y in 0..(GRID_SIZE.y + 1) {
+        board.push(Block {
+            index: 7,
+            pos: ivec2(0, y),
+        });
+        board.push(Block {
+            index: 7,
+            pos: ivec2(GRID_SIZE.x + 1, y),
+        });
+    }
+    for x in 0..(GRID_SIZE.x) {
+        board.push(Block {
+            index: 7,
+            pos: ivec2(x + 1, GRID_SIZE.y),
+        });
+    }
+    board
 }
 
 fn get_rect_from_index(index: i32) -> Option<Rect> {
--- src/pieces.rs
+++ src/pieces.rs
@@ -1,21 +1,64 @@
 use macroquad::math::IVec2;
 use crate::*;
 
-const NULL_PIECE: Piece = Piece {
-    index: -1,
-    rotation: -1,
-    pos: IVec2::ZERO,
-};
+//const NULL_PIECE: Piece = Piece {
+//    index: -1,
+//    rotation: -1,
+//    pos: IVec2::ZERO,
+//};
 const START_POS: IVec2 = IVec2 { x: 4, y: 0 };
 
-struct Piece {
+#[derive(Clone, Copy)]
+pub struct Piece {
     pub index: i8,
     pub rotation: i8,
+    pub pos: IVec2,
+} impl Piece {
+    pub fn add_to_board(self, board: &mut Vec<Block>) {
+        unsafe{
+            for pos in SRS_DATA["pieces"][self.index as usize][self.rotation as usize].as_array().unwrap() {
+                let x = pos[0].as_i64().unwrap() as i32 + self.pos.x;
+                let y = pos[1].as_i64().unwrap() as i32 + self.pos.y;
+                board.push(Block {
+                    index: self.index,
+                    pos: IVec2 { x, y },
+                });
+            }
+        }
+    }
+    // also does rotation checking
+    pub fn can_move(self, board: &[Block]) -> bool {
+        unsafe{
+            for pos in SRS_DATA["pieces"][self.index as usize][self.rotation as usize].as_array().unwrap() {
+                let x = pos[0].as_i64().unwrap() as i32 + self.pos.x;
+                let y = pos[1].as_i64().unwrap() as i32 + self.pos.y;
+                if !(0..10).contains(&x) || y >= 20 {
+                    return false;
+                }
+                for block in board.iter() {
+                    if block.pos == (IVec2 { x, y }) {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+    pub fn copy(self) -> Piece {
+        Piece {
+            index: self.index,
+            rotation: self.rotation,
+            pos: self.pos,
+        }
+    }
+}
+pub struct Block {
+    pub index: i8,
     pub pos: IVec2,
 }
 
 static mut ACTIVE_PIECE: Piece = Piece {
-    index: 5,
+    index: 0,
     rotation: 0,
     pos: START_POS,
 };
@@ -28,26 +71,49 @@ pub fn load_json() {
     }
 }
 
-pub fn update(texture: &Texture2D) {
+pub fn update(texture: &Texture2D, block_size: f32, offset_x: f32, board: &mut Vec<Block>) {
     unsafe{
+        let mut future_piece = ACTIVE_PIECE.copy();
         if is_key_pressed(KeyCode::A) {
-            ACTIVE_PIECE.pos.x -= 1;
+            future_piece.pos.x -= 1;
         } else if is_key_pressed(KeyCode::D) {
-            ACTIVE_PIECE.pos.x += 1;
+            future_piece.pos.x += 1;
+        }
+        if is_key_pressed(KeyCode::S) {
+            future_piece.pos.y += 1;
+        }
+        if is_key_pressed(KeyCode::Left){
+            future_piece.rotation += 3;
+            future_piece.rotation %= 4;
+        } else if is_key_pressed(KeyCode::Right) {
+            future_piece.rotation += 1;
+            future_piece.rotation %= 4;
+        }
+        if future_piece.can_move(board) {
+            ACTIVE_PIECE = future_piece;
+        }
+
+        if ACTIVE_PIECE.pos.y > 10 {
+            ACTIVE_PIECE.add_to_board(board);
+            ACTIVE_PIECE = Piece {
+                index: 0,
+                rotation: 0,
+                pos: START_POS,
+            };
         }
 
         for pos in SRS_DATA["pieces"][ACTIVE_PIECE.index as usize][ACTIVE_PIECE.rotation as usize].as_array().unwrap() {
             let x = pos[0].as_i64().unwrap() as i32 + ACTIVE_PIECE.pos.x;
             let y = pos[1].as_i64().unwrap() as i32 + ACTIVE_PIECE.pos.y;
             let params = DrawTextureParams {
-                dest_size: Some(vec2(BLOCK_SIZE, BLOCK_SIZE)),
+                dest_size: Some(vec2(block_size, block_size)),
                 source: get_rect_from_index(ACTIVE_PIECE.index.into()),
                 ..Default::default()
             };
             draw_texture_ex(
                 texture,
-                OFFSET_X + x as f32 * BLOCK_SIZE,
-                y as f32 * BLOCK_SIZE,
+                offset_x + x as f32 * block_size,
+                y as f32 * block_size,
                 WHITE,
                 params,
             );
