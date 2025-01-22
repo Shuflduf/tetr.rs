85c2388feb4d15680e0f6a29ee8edcb781c77fdb
Shuflduf <shuflduf@gmail.com>
2025-01-21 18:22:44 -0700

    formating i think

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,28 @@ use macroquad::prelude::*;
 #[macroquad::main("MyGame")]
 async fn main() {
     const GRID_SIZE: IVec2 = ivec2(10, 20);
+    let texture = load_texture("assets/texture_simple.png").await.unwrap();
+
+    let bounds: Vec<IVec2> = {
+        let mut final_bounds = Vec::new();
+        for y in 0..(GRID_SIZE.y + 1) {
+            final_bounds.push(ivec2(0, y));
+            final_bounds.push(ivec2(GRID_SIZE.x + 1, y));
+        }
+        for x in 0..(GRID_SIZE.x) {
+            final_bounds.push(ivec2(x + 1, GRID_SIZE.y));
+        }
+        final_bounds
+    };
     loop {
         clear_background(BLACK);
 
         // Draw board
-        let bounds: Vec<IVec2> = {
-            let mut final_bounds = Vec::new();
-            for y in 0..(GRID_SIZE.y + 1) {
-                final_bounds.push(ivec2(
-                    0,
-                    y,
-                ));
-                final_bounds.push(ivec2(
-                    GRID_SIZE.x + 1,
-                    y,
-                ));
-            }
-            for x in 0..(GRID_SIZE.x) {
-                final_bounds.push(ivec2(
-                    x + 1,
-                    GRID_SIZE.y,
-                ));
-            }
-            final_bounds
-        };
-
         let block_size = screen_height() / (GRID_SIZE.y as f32 + 1.0);
-        let texture = load_texture("assets/texture_simple.png").await.unwrap();
         for block in bounds.iter() {
             let params = DrawTextureParams {
                 dest_size: Some(vec2(block_size, block_size)),
-                source: get_rect_from_index(6),
+                source: get_rect_from_index(7),
                 ..Default::default()
             };
             draw_texture_ex(
@@ -41,7 +32,7 @@ async fn main() {
                 block.x as f32 * block_size,
                 block.y as f32 * block_size,
                 WHITE,
-                params
+                params,
             );
         }
 
@@ -55,10 +46,5 @@ async fn main() {
 }
 
 fn get_rect_from_index(index: i32) -> Option<Rect> {
-    Some(Rect::new(
-        index as f32 * 32.0,
-        0.0,
-        32.0,
-        32.0,
-    ))
+    Some(Rect::new(index as f32 * 32.0, 0.0, 32.0, 32.0))
 }
